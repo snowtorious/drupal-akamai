a91071993ad99d884631fd11732915fa716b4a67
Elijah Lynn <elijah@elijahlynn.net>
2016-02-08 08:30:57 -0500

    Don't escape slashes in objects.
    It is unneccessary to escape slashes and increases the amount of data in
    the request.

--- src/Ccu3Client.php
+++ src/Ccu3Client.php
@@ -47,4 +47,7 @@ class Ccu3Client extends BaseCcuClient implements CcuClientInterface {
     // Force a non-associative array for json_encode().
     $purge_body['objects'] = array_values($purge_body['objects']);
 
+    // Use JSON_UNESCAPED_SLASHES to reduce amount of data in request body.
+    return json_encode($purge_body, JSON_UNESCAPED_SLASHES);
+  }
 }
